ef9f048fddb00373bd8361f0639842c8d2499aa2
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-27 09:28:08 +0000

    Workingdevotional test

--- app/layout.js
+++ app/layout.js
@@ -8,17 +8,6 @@ const raleway = Raleway({
   subsets: ['latin']
 })
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata = {
   title: "Devotion",
   description: "This is a devotional application",
