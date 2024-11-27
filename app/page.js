c15f6ba4c181ad21338cb21c87ca928074937af1
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-27 09:49:16 +0000

    working on navbar

--- app/page.js
+++ app/page.js
@@ -1,21 +1,16 @@
-'use client'
-import { useState } from 'react';
-import MainVideoTab from './components/MainVideoTab';
-import DevotionTab from './components/DevotionTab';
-import Navbar from './components/Navbar';
+"use client";
+import { useState } from "react";
+import MainVideoTab from "./components/MainVideoTab";
+import DevotionTab from "./components/DevotionTab";
+import Navbar from "./components/Navbar";
 export default function Home() {
-  const [activeTab, setActiveTab] = useState('devotional')
+  const [activeTab, setActiveTab] = useState("devotional");
 
   return (
     <div>
-      {activeTab === 'home' &&(
-        <MainVideoTab/>
-      )}
-      {activeTab === 'devotional' &&(
-        <DevotionTab/>
-      )}
-      <Navbar activeTab={activeTab} setActiveTab={setActiveTab}/>
+      {activeTab === "home" && <MainVideoTab />}
+      {activeTab === "devotional" && <DevotionTab />}
+      <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
     </div>
-
   );
 }
