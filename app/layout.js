bf0a34624150d592f14a6108892be1208166fc33
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-27 09:34:17 +0000

    Working on the navbar

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,9 @@
-import {Raleway} from 'next/font/google'
+import { Raleway } from "next/font/google";
 import "./globals.css";
 
-
-
 const raleway = Raleway({
-  subsets: ['latin']
-})
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Devotion",
@@ -15,11 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${raleway.className} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${raleway.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
-
+'use client'
+import { useState } from 'react';
 export default function Home() {
+  const [activeTab, setActiveTab] = useState('home')
+
   return (
     <div>Landing Page</div>
   );
