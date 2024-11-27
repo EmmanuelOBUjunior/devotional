2d5fe05897fa011d307b79b11c1b83d698863a88
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-27 09:59:38 +0000

    Working on the navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 const Navbar = ({activeTab, setActiveTab}) => {
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-white shadow-md p-3 flex justify-around items-center md:hidden">
-      <button className="flex items-center gap-2">
+      <button onClick={()=>setActiveTab('home')} className="flex items-center gap-2">
         <Image src="icons/victorylogo.svg" alt="Home" width={24} height={24} />
         <span className="text-sm">Home</span>
       </button>
-      <button className="flex items-center gap-2">
+      <button onClick={()=>setActiveTab('devotional')} className="flex items-center gap-2">
         <Image src="icons/file.svg" alt="Devotion" width={24} height={24}/>
         <span className="text-sm">Devotion</span>
       </button>
