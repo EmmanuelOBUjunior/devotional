5be766746f58c673a904ffb5bef13c05cb613d79
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-27 09:58:36 +0000

    Working on the navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Navbar = ({activeTab, setActiveTab}) => {
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-white shadow-md p-3 flex justify-around items-center md:hidden">
-      <div href="/" className="flex items-center gap-2">
+      <div className="flex items-center gap-2">
         <Image src="icons/victorylogo.svg" alt="Home" width={24} height={24} />
         <span className="text-sm">Home</span>
       </div>
