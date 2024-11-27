850c25e5a570c02c40597f1598f9217bc20e179f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-27 15:37:02 +0000

    working on the mainvideo tab

--- app/components/Connect.jsx
+++ app/components/Connect.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Connect = () => {
   return (
-    <div>Connect</div>
+    <div className="flex-1 bg-gray-100 p-4 text-center rounded-md shadow-md">
+        <h2 className="text-lg font-bold">Share with Your Community</h2>
+        {/* Add content or links here */}
+      </div>
   )
 }
 
