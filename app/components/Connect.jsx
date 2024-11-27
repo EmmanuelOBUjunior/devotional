8f514281926e92c9f588a75f160e50b6e3709704
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-27 15:37:08 +0000

    working on the mainvideo tab

--- app/components/Connect.jsx
+++ app/components/Connect.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Connect = () => {
   return (
     <div className="flex-1 bg-gray-100 p-4 text-center rounded-md shadow-md">
-        <h2 className="text-lg font-bold">Share with Your Community</h2>
-        {/* Add content or links here */}
-      </div>
-  )
-}
+      <h2 className="text-lg font-bold">Share with Your Community</h2>
+      {/* Add content or links here */}
+    </div>
+  );
+};
 
-export default Connect
+export default Connect;
