7d93cb6b187ca7c39b318849d75f24441112c33f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-27 15:36:45 +0000

    working on the mainvideo tab

--- app/components/Share.jsx
+++ app/components/Share.jsx
@@ -4,7 +4,7 @@ const Share = () => {
   return (
     <div className="flex-1 bg-gray-100 p-4 text-center rounded-md shadow-md">
       <h2 className="text-lg font-bold">Connect with Us</h2>
-      icons here
+      Connect icons here
     </div>
   );
 };
