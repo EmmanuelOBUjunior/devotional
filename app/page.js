1a92c1aaa10083d9bcbe2ae513b604af08da1262
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-27 09:38:23 +0000

    Working on the tab

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
 'use client'
 import { useState } from 'react';
+import MainVideoTab from './components/MainVideoTab';
+import DevotionTab from './components/DevotionTab';
 export default function Home() {
   const [activeTab, setActiveTab] = useState('home')
 
   return (
-    <div>Landing Page</div>
+    <div>
+      {activeTab === 'home' &&(
+        <MainVideoTab/>
+      )}
+      {activeTab === 'devotional' &&(
+        <DevotionTab/>
+      )}
+    </div>
+
   );
 }
