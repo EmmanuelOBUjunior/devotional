cc362c34fbd1bf21c170b704da6f810d16141bc3
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-27 14:57:13 +0000

    Working on the navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ activeTab, setActiveTab }) => {
     <>
       {/* Desktop Navigation */}
       <div className="fixed bottom-4 left-0 right-0 flex justify-center">
-        <nav className="mx-auto flex bg-white shadow-md rounded-full p-4 space-x-20 items-center">
+        <nav className="mx-auto flex bg-white shadow-md rounded-full p-4 space-x-16 items-center">
           <button
             onClick={() => setActiveTab("home")}
             className={`flex items-center gap-2 font-bold ${
