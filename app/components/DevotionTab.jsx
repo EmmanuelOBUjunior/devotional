3376d4e8f13f0c955c019e2565850c0d51075aab
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-27 09:36:22 +0000

    Added both the mainvideo tab and mainvideotab

---
+++ app/components/DevotionTab.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DevotionTab = () => {
+  return (
+    <div>DevotionTab</div>
+  )
+}
+
+export default DevotionTab
---
+++ app/components/MainVideoTab.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainVideoTab = () => {
+  return (
+    <div>MainVideoTab</div>
+  )
+}
+
+export default MainVideoTab
