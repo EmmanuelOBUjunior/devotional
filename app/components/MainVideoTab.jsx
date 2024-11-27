6d7ac72261decafc9f8dd1c8320cce464e0a1508
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-27 15:30:16 +0000

    Working on the navbar

--- app/components/MainVideoTab.jsx
+++ app/components/MainVideoTab.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const MainVideoTab = () => {
   return (
         <div className="min-h-screen flex flex-col justify-between pb-16">
-      <div className="flex-1 flex items-center justify-center bg-black">
+      <div className="flex m-auto flex-col items-center justify-center">
         <video 
           className="w-full max-w-md md:max-w-3xl" 
           controls 
           src="video/homevideo.mp4" 
         />
-      </div>
-
       <div className="w-full">
         share and connect here
       </div>
+      </div>
+
     </div>
   )
 }
