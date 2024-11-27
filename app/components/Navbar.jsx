ce887e21106b9045fc173b9e671fca613b51adb1
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-27 14:45:32 +0000

    Working on the navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,7 +5,8 @@ const Navbar = ({activeTab, setActiveTab}) => {
   return (
     <>
     {/* Desktop Navigation */}
-    <div className="fixed bottom-3 left-0 right-0 bg-white shadow-md p-3 mx-4 rounded-full justify-around items-center flex border-2">
+    <div className="fixed bottom-4 left-0 right-0 bg-white shadow-md p-3 mx-4 rounded-full justify-around items-center flex border-2">
+        <nav>
       <button onClick={()=>setActiveTab('home')} className={`flex items-center gap-2 font-bold ${activeTab === 'home'?'':''}`}>
         <Image src="icons/victorylogo.svg" alt="Home" width={24} height={24} />
         <span className="text-sm">Home</span>
@@ -14,6 +15,7 @@ const Navbar = ({activeTab, setActiveTab}) => {
         <Image src="icons/file.svg" alt="Devotion" width={24} height={24}/>
         <span className="text-sm">Devotion</span>
       </button>
+        </nav>
     </div>
    
     </>
