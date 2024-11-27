673c5e6edba955d3b71ebeaf76244fac752cfbc3
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-27 10:55:20 +0000

    Working on the navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = ({activeTab, setActiveTab}) => {
   return (
     <>
     {/* Desktop Navigation */}
-    <div className="fixed bottom-3 left-0 right-0 bg-white shadow-md p-3 mx-4 rounded-full flex justify-around items-center md:hidden border-2">
+    <div className="fixed bottom-3 left-0 right-0 bg-white shadow-md p-3 mx-4 rounded-full justify-around items-center flex border-2">
       <button onClick={()=>setActiveTab('home')} className={`flex items-center gap-2 font-bold ${activeTab === 'home'?'':''}`}>
         <Image src="icons/victorylogo.svg" alt="Home" width={24} height={24} />
         <span className="text-sm">Home</span>
@@ -15,17 +15,7 @@ const Navbar = ({activeTab, setActiveTab}) => {
         <span className="text-sm">Devotion</span>
       </button>
     </div>
-    {/* Mobile Navigation */}
-    <div className="fixed bottom-3 left-0 right-0 bg-white shadow-md p-3 mx-4 rounded-full flex justify-around items-center">
-      <button onClick={()=>setActiveTab('home')} className={`flex items-center gap-2 font-bold ${activeTab === 'home'?'':''}`}>
-        <Image src="icons/victorylogo.svg" alt="Home" width={24} height={24} />
-        <span className="text-sm">Home</span>
-      </button>
-      <button onClick={()=>setActiveTab('devotional')} className="flex items-center gap-2 font-bold">
-        <Image src="icons/file.svg" alt="Devotion" width={24} height={24}/>
-        <span className="text-sm">Devotion</span>
-      </button>
-    </div>
+   
     </>
   )
 }
