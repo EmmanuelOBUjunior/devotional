f6d5a61a553e96ad1bcf4ed417b421e1782945ab
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-28 13:23:16 +0000

    Working on the meta tags

--- app/components/MainVideoTab.jsx
+++ app/components/MainVideoTab.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Connect from './Connect'
 import Share from './Share'
+import MetaTags from './MetaTags'
 
 
 const MainVideoTab = () => {
   return (
         <div className="min-h-screen flex flex-col px-4 justify-between pb-16">
+          <MetaTags url='https://devotional-nu.vercel.app/' description='This is the devotion for Hearing the word of God' imageURL='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3JWm2Ai7KPqpxZLPmrrxT88LL20pfnTF6jw&s'/>
       <div className="flex m-auto flex-col items-center justify-center">
         <video 
           className="w-full max-w-md md:max-w-3xl" 
