e63f5f6e1ab47381b6d6675ce891ba7b4ccac477
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-28 09:22:10 +0000

    working on the devotion tab

--- app/components/MainVideoTab.jsx
+++ app/components/MainVideoTab.jsx
@@ -11,7 +11,7 @@ const MainVideoTab = () => {
           controls 
           src="video/homevideo.mp4" 
         />
-      <div className="bg-blue-500 w-full flex flex-col md:flex-row md:px-4 items-center gap-3 p-2 rounded-lg mt-2 justify-between">
+      <div className="bg-[#1837C2] w-full flex flex-col md:flex-row md:px-4 items-center gap-3 p-2 rounded-lg mt-2 justify-between">
         <Connect/>
         <Share/>
       </div>
