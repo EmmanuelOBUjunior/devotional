449c628713534516a2108044fdcc0e021be96d86
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-28 21:20:07 +0000

    working on the metadata

--- app/components/MainVideoTab.jsx
+++ app/components/MainVideoTab.jsx
@@ -6,12 +6,6 @@ import MetaTags from "./MetaTags";
 const MainVideoTab = () => {
   return (
     <div className="min-h-screen flex flex-col px-4 justify-between pb-16">
-      <MetaTags
-        url="https://devotional-nu.vercel.app/"
-        description="This is the devotion for Hearing the word of God"
-        imageURL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3JWm2Ai7KPqpxZLPmrrxT88LL20pfnTF6jw&s"
-        title='Hearing the voice of God'
-      />
       <div className="flex m-auto flex-col items-center justify-center">
         <video
           className="w-full max-w-md md:max-w-3xl"
