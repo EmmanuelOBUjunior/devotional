973e2a5c9652205c2eaff8374a144c498ea88086
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-28 13:17:48 +0000

    Working on the meta tags

--- app/components/MetaTags.jsx
+++ app/components/MetaTags.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import Head from 'next/head'
 
-const MetaTags = () => {
+const MetaTags = ({title, imageURL, description, url}) => {
   return (
-    <div>MetaTags</div>
+    <Head>MetaTags</Head>
   )
 }
 
