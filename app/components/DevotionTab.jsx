977c2b61127d43111ee797d665c42b6c97b5ffc1
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-28 08:14:32 +0000

    working on the devotion tab

--- app/components/DevotionTab.jsx
+++ app/components/DevotionTab.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const DevotionTab = () => {
   return (
     <div className='flex flex-col max-h-fit p-4 max-w-4xl mx-auto'>
+        <div className='mb-4'>
+    <div className='w-full h-60 bg-gray-200 rounded-md'>
 
+    </div>
+        </div>
     </div>
   )
 }
