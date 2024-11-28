16977f79acb21083abe3d9ff63ea8d8069bf765b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-28 08:20:21 +0000

    working on the devotion tab

--- app/components/DevotionTab.jsx
+++ app/components/DevotionTab.jsx
@@ -14,7 +14,7 @@ const DevotionTab = () => {
       </div>
       <div className="flex flex-col md:flex-row gap-6 flex-grow">
         <div className="flex flex-col bg-white p-6 shadow-md md:w-2/3 rounded-md">
-        <div>
+        <div className="flex items-center justify-between">
             <h2 className="font-extrabold uppercase">Hearing God&apos;s Voice</h2>
         </div>
         </div>
