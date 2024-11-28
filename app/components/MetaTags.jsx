b814fc2c1d8a69154f792d1dfd48bad72bc0625c
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-28 13:18:22 +0000

    Working on the meta tags

--- app/components/MetaTags.jsx
+++ app/components/MetaTags.jsx
@@ -2,7 +2,23 @@ import Head from 'next/head'
 
 const MetaTags = ({title, imageURL, description, url}) => {
   return (
-    <Head>MetaTags</Head>
+    <Head>
+        <title>{title}</title>
+      <meta name="description" content={description} />
+
+      {/* Open Graph (OG) for Facebook, LinkedIn, etc. */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter Card */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+    </Head>
   )
 }
 
