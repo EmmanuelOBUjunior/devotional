34ab83b5b7104f8187e9549b84c2ac456906e039
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-28 10:55:55 +0000

    working on the share functionality

--- app/components/MainVideoTab.jsx
+++ app/components/MainVideoTab.jsx
@@ -12,7 +12,7 @@ const MainVideoTab = () => {
         <meta property="og:title" content="Hearing the voice of God" />
         <meta property="og:description" content="This is the devotion from Victory Church" />
         <meta property="og:url" content="https://devotional-nu.vercel.app/" />
-        <meta property="og:image" content="https://yourwebsite.com/yourImage.jpg" />
+        <meta property="og:image" content="/public/icons/victorylogo.png" />
       </Head>
         <div className="min-h-screen flex flex-col px-4 justify-between pb-16">
       <div className="flex m-auto flex-col items-center justify-center">
