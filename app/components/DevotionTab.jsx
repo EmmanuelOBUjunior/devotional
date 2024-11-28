983e32bc986a5d94e329312288e9c0e68cbb56af
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-28 08:15:39 +0000

    working on the devotion tab

--- app/components/DevotionTab.jsx
+++ app/components/DevotionTab.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const DevotionTab = () => {
   return (
-    <div className='flex flex-col max-h-fit p-4 max-w-4xl mx-auto'>
-        <div className='mb-4'>
-    <div className='w-full h-60 bg-gray-200 rounded-md'>
-
-    </div>
+    <div className="flex flex-col max-h-fit p-4 max-w-4xl mx-auto">
+      <div className="mb-4">
+        <div className="w-full h-60 bg-gray-200 rounded-md">
+          <video
+            className="w-full max-w-md md:max-w-3xl"
+            controls
+            src="video/homevideo.mp4"
+          />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DevotionTab
+export default DevotionTab;
